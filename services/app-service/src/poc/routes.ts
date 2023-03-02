b3650fb1e047045fb25f931c9698cf03b5b8b874
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-03-02 14:48:19 -0800

    Remove console logs

--- services/app-service/src/poc/routes.ts
+++ services/app-service/src/poc/routes.ts
@@ -14,14 +14,10 @@ router.post(
   handleValidationErrors,
   async (req: Request, res: Response) => {
     const session = req.session;
-    console.log("sessionId", session.id);
-    console.log("starting", session.chatSessionMessages);
     // Generate the request to the ChatGPT model
     const messages = await createGPTRequestFromPrompt(req.body.Body, session);
-    console.log("request generated", session.chatSessionMessages);
     // Get response from ChatGPT
     const completion = await getChatGPTCompletion(messages, session);
-    console.log("ending", session.chatSessionMessages);
     const twiml = new MessagingResponse();
     if (completion?.content) {
       twiml.message(completion?.content);
