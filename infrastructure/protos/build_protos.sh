9417c02471006eb186650da02a9ee276e6b4be6d
bcormier1 <bcormier@uci.edu>
2023-03-04 18:16:34 -0800

    generate protos, first pass

--- infrastructure/protos/build_protos.sh
+++ infrastructure/protos/build_protos.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 cd "${0%/*}"
-protoc --proto_path=../../services/inference-service/protos --python_out=generated protos/inference_service.proto
+protoc --proto_path=../../services/inference-service/protos --python_out=../../services/inference-service/generated_protos inference_service.proto
