141da1a984af1e7a3887851143249e749ec9b50c
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-03-18 16:46:41 -0700

    Chats working great - 5 min expiration, context and all, stored in Postgres

--- services/app-service/src/chat/service.ts
+++ services/app-service/src/chat/service.ts
@@ -1,17 +1,10 @@
-import bcrypt from "bcryptjs";
-import validator from "validator";
-import { BadRequestError, InternalError } from "../utils/errors/index.js";
+import { InternalError, UnauthorizedError } from "../utils/errors/index.js";
 import { PineconeClient } from "@pinecone-database/pinecone";
-import { JudieSession } from "../utils/express.js";
-import {
-  Configuration,
-  OpenAIApi,
-  ChatCompletionRequestMessage,
-  ChatCompletionResponseMessage,
-} from "openai";
+import { Configuration, OpenAIApi, ChatCompletionRequestMessage } from "openai";
 import * as openai from "openai";
 import dbClient from "../utils/prisma.js";
-import { Chat, Message, MessageType } from "@prisma/client";
+import { Chat, Message, MessageType, Prisma } from "@prisma/client";
+import { ChatAndMessageResponse } from "./types.js";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY || "",
@@ -25,6 +18,7 @@ await pinecone.init({
 });
 
 const OPENAI_PROMPT_CHAR_LIMIT = 6000;
+const OPENAI_COMPLETION_MODEL = "gpt-4-0314";
 
 const getChatCompletionRequestMessageRoleEnumFromMessageType = (
   messageType: MessageType
@@ -49,51 +43,158 @@ const transformMessageToChatCompletionMessage = (
   };
 };
 
-export const createGPTRequestFromPrompt = async (
-  userId: string,
-  prompt: string
-): Promise<ChatCompletionRequestMessage[]> => {
-  try {
-    // Chat object we will be working with
-    let chat: Chat | (Chat & { messages: Message[] }) | null = null;
-    // Find the most recently updated chat for this user
-    let existingChat = await dbClient.chat.findFirst({
-      where: {
-        userId,
-      },
-      include: {
-        messages: {
-          orderBy: {
-            createdAt: "desc",
-          },
-        },
-      },
-      orderBy: {
-        updatedAt: "desc",
-      },
-      take: 1,
-    });
-    chat = existingChat;
+const createChat = async (params: Prisma.ChatCreateInput) => {
+  const newChat = await dbClient.chat.create({
+    data: {
+      ...params,
+    },
+    include: {
+      messages: true,
+    },
+  });
+  return newChat;
+};
 
-    // Test if:
-    // 1. existingChat exists
-    // 2. existingChat has been updated in the past 5 minutes
-    const chatUpdatedWithin5Minutes =
-      existingChat &&
-      existingChat.updatedAt &&
-      new Date().getTime() - new Date(existingChat.updatedAt).getTime() <
-        5 * 60 * 1000;
-    // If not, create a new chat
-    if (!chatUpdatedWithin5Minutes) {
-      const newChat = await dbClient.chat.create({
-        data: {
-          userId,
+const updateChat = async (chatId: string, params: Prisma.ChatUpdateInput) => {
+  const newChat = await dbClient.chat.update({
+    where: {
+      id: chatId,
+    },
+    data: params,
+    include: {
+      messages: {
+        orderBy: {
+          createdAt: "desc",
         },
-      });
-      chat = newChat;
+      },
+    },
+  });
+  return newChat;
+};
+
+const createMessage = async (
+  message: ChatCompletionRequestMessage,
+  chat: Chat
+) => {
+  const { role, content } = message;
+  const type = role === "user" ? MessageType.USER : MessageType.BOT;
+  const newMessage = await dbClient.message.create({
+    data: {
+      content,
+      type,
+      chatId: chat.id,
+    },
+  });
+  return newMessage;
+};
+
+/**
+ *
+ * @remarks
+ * This method retrieves a currently active chat and its messages for a user.
+ * If no chat is found, one is created.
+ *
+ * @param userId - ID of the user
+ * @returns ChatAndMessageResponse
+ *
+ * @beta
+ */
+export const getChatAndMessagesForUser = async (
+  userId: string | undefined,
+  newChat: boolean = false
+): Promise<ChatAndMessageResponse> => {
+  if (!userId) {
+    throw new UnauthorizedError("User not found");
+  }
+  if (newChat) {
+    return await createChat({ user: { connect: { id: userId } } });
+  }
+  const chat = await dbClient.chat.findFirst({
+    where: {
+      userId,
+    },
+    include: {
+      messages: {
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    take: 1,
+  });
+  // If chat exists
+  if (chat) {
+    // And if it's been updated in the last 5 minutes
+    // Or created in the past 5 minutes
+    const FIVE_MINUTES_MS = 5 * 60 * 1000;
+    const chatUpdatedWithin5Minutes = chat.updatedAt
+      ? new Date().getTime() - new Date(chat.updatedAt).getTime() <
+        FIVE_MINUTES_MS
+      : new Date().getTime() - new Date(chat.createdAt).getTime() <
+        FIVE_MINUTES_MS;
+    // If so, use this current chat and messages
+    if (chatUpdatedWithin5Minutes) {
+      return chat;
+    } else {
+      // If not, create a new chat and return it with no messages
+      return await createChat({ user: { connect: { id: userId } } });
+    }
+  } else {
+    // If user has no chats, create a new chat and return it with no messages
+    return await createChat({ user: { connect: { id: userId } } });
+  }
+};
+
+/**
+ *
+ * @remarks
+ * This method takes an existing chat and its messages,
+ * Generates a new prompt from the existing chat and its messages
+ * and then saves that prompt to the DB.
+ *
+ * @param userId - ID of the user
+ * @param prompt - The prompt the user just supplied
+ * @param chat - ChatAndMessageResponse
+ * @returns ChatAndMessageResponse
+ *
+ * @beta
+ */
+export const createGPTRequestFromPrompt = async ({
+  userId,
+  prompt,
+  chat,
+}: {
+  userId: string | undefined;
+  prompt: string;
+  chat: ChatAndMessageResponse;
+}): Promise<ChatAndMessageResponse> => {
+  try {
+    if (!userId) {
+      throw new UnauthorizedError("User Not Found");
     }
 
-    const chatMessages = existingChat?.messages || [];
+    // Build messages array
+    const existingMessages = chat.messages;
+    const newMessages = [];
+    let messages = existingMessages || [];
+    // If we haven't given a system prompt yet, give one
+    if (messages.length === 0) {
+      const promptStart =
+        "You are an advanced AI Tutor named Judie that always responds in the Socratic style, and students are asking you questions.\n\
+      You will get the user question as well as any relevant context to answer that question.\n\
+      I want you to provide an in-depth answer to the question and guide students to the appropriate answer.\n\
+      Your tone is informal, and didactic, and you want to explain things so the largest audience can understand them. \n\
+      You should always tune your question to the interest & knowledge of the student, breaking down the problem into simpler parts until it's just the right level for them. \n\n";
+
+      newMessages.push({
+        content: promptStart,
+        type: MessageType.BOT,
+        createdAt: new Date(),
+      });
+    }
 
     // Get embedding vector from OpenAI
     const embeddingResponse = await openaiClient.createEmbedding({
@@ -101,7 +202,6 @@ export const createGPTRequestFromPrompt = async (
       model: "text-embedding-ada-002",
     });
     const embeddingVector = embeddingResponse.data?.data?.[0]?.embedding;
-
     // Get matching vectors from Pinecone
     const pcIndex = pinecone.Index("apmvp");
     const pineconeResponse = await pcIndex.query({
@@ -110,100 +210,156 @@ export const createGPTRequestFromPrompt = async (
         topK: 3,
         includeValues: true,
         includeMetadata: true,
-        filter: {
-          range: {
-            score: {
-              min: 0.8,
-            },
-          },
+      },
+    });
+    console.log("matches", pineconeResponse.matches);
+    const matches = pineconeResponse.matches;
+    // Get metadata from each matching vector
+    const matchMetadatas =
+      matches
+        ?.filter(
+          (match) =>
+            match &&
+            !!(match.metadata as { [key: string]: string }).Sentence &&
+            (match?.score || 0) > 0.8
+        )
+        ?.map(
+          (match) => (match.metadata as { [key: string]: string }).Sentence
+        ) || [];
+    // Build latest prompt object
+    let promptChunks = [];
+    if (matchMetadatas.length > 0) {
+      promptChunks.push("Context: \n");
+      for (const matchMetadata of matchMetadatas) {
+        promptChunks.push(`${matchMetadata}\n`);
+      }
+    }
+
+    promptChunks.push("Question: \n");
+    promptChunks.push(`${prompt}\n`);
+    const currentPrompt = promptChunks.join("");
+
+    // Push this prompt to messages arr
+    newMessages.push({
+      content: currentPrompt,
+      type: MessageType.USER,
+      createdAt: new Date(),
+    });
+
+    // Update chat with new messages
+    const newMessagesMapped: Prisma.MessageCreateManyChatInput[] =
+      newMessages.map((m) => ({
+        content: m.content,
+        type: m.type,
+        createdAt: m.createdAt,
+      }));
+
+    const newChat = await updateChat(chat.id, {
+      messages: {
+        createMany: {
+          // Using 'as' because we technically have a partial above
+          // but we know content is defined, which is the only concern here
+          data: newMessagesMapped,
         },
       },
     });
-    const matches = pineconeResponse.matches;
-
-    // Get metadata from each matching vector
-    const matchMetadatas =
-      matches?.map(
-        (match) => (match.metadata as { [key: string]: string }).text
-      ) || [];
-
-    // Build messages array
-    let messages: ChatCompletionRequestMessage[] = [];
-    // If we haven't given a system prompt yet, give one
-    if (chatMessages.length === 0) {
-      const promptStart =
-        "You are an advanced AI Tutor named Judie that always responds in the Socratic style, and students are asking you questions.\n\
-      You will get the user question as well as relevant context to answer that question.\n\
-      I want you to provide an in-depth answer to the question and guide students to the appropriate answer.\n\
-      Your tone is informal, and didactic, and you want to explain things so the largest audience can understand them. \n\
-      You should always tune your question to the interest & knowledge of the student, breaking down the problem into simpler parts until it's just the right level for them. \n\n";
-      messages.push({
-        content: promptStart,
-        role: "system",
-      });
-    }
-
-    // Build latest prompt object
-    let promptChunks = [];
-    promptChunks.push("Context: \n");
-    for (const matchMetadata of matchMetadatas) {
-      promptChunks.push(`${matchMetadata}\n`);
-    }
-    promptChunks.push("Question: \n");
-    promptChunks.push(`${prompt}\n`);
-
-    const currentPrompt = promptChunks.join("");
-
-    // Add user prompt and previous messages up to limit
-    let charCountAllMessages = currentPrompt.length;
-    // Loop over previous messages in reverse order (going up in the chat log)
-    for (const existingMessage of chatMessages) {
-      // Check if adding it to the existing arr of messages will exceed char limit
-      const existingMessageLen = existingMessage.content.length;
-      // If it will, break
-      if (
-        charCountAllMessages + existingMessageLen >
-        OPENAI_PROMPT_CHAR_LIMIT
-      ) {
-        break;
-      }
-      // If it won't, add it to the arr of messages
-      // And add its length to the total char count
-      messages.push(transformMessageToChatCompletionMessage(existingMessage));
-      charCountAllMessages += existingMessageLen;
-    }
-
-    // Push this prompt to messages arr
-    messages.push({
-      content: currentPrompt,
-      role: "user",
-    });
-
-    return messages;
+    return newChat;
   } catch (err) {
     console.error("Error building prompt: ", err);
     throw err;
   }
 };
 
+/**
+ * @remarks
+ * This method takes an existing chat and its messages,
+ * Generates a new prompt from the existing chat and its messages
+ * sends the prompt to OpenAI to get a completion
+ * and then saves that completion to the DB.
+ * @param userId - ID of the user
+ * @param chat - ChatAndMessageResponse
+ * @returns ChatAndMessageResponse
+ *
+ * @beta
+ */
 export const getChatGPTCompletion = async (
-  messages: ChatCompletionRequestMessage[]
-): Promise<ChatCompletionResponseMessage | undefined> => {
+  chat: Chat & { messages: Message[] }
+): Promise<(Chat & { messages: Message[] }) | undefined> => {
   try {
+    const transformedMessages = chat.messages.map((message: Message) =>
+      transformMessageToChatCompletionMessage(message)
+    );
+
+    let currentMessagesContentLength = 0;
+    const maxLengthLimitedMessages: ChatCompletionRequestMessage[] =
+      transformedMessages.reduce(
+        (
+          acc: ChatCompletionRequestMessage[],
+          val: ChatCompletionRequestMessage
+        ) => {
+          if (
+            currentMessagesContentLength + val.content.length >
+            OPENAI_PROMPT_CHAR_LIMIT
+          ) {
+            return acc;
+          } else {
+            currentMessagesContentLength += val.content.length;
+            return [...acc, val];
+          }
+        },
+        []
+      );
     const completion = await openaiClient.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages,
+      model: OPENAI_COMPLETION_MODEL,
+      messages: maxLengthLimitedMessages,
+      user: chat.userId,
       temperature: 1.0,
-      max_tokens: 1200,
+      max_tokens: 800,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     });
-    // console.log("Completion", completion.data.choices[0].message?.content);
-    if (completion.data.choices[0].message?.content) {
-      // Save new completion to DB
+    const completionMessageContent =
+      completion.data.choices[0].message?.content;
 
-      return completion.data.choices[0].message;
+    if (completionMessageContent) {
+      const newMessage = {
+        content: completionMessageContent,
+        type: MessageType.BOT,
+      };
+      // Save new completion to DB
+      const newChat = await updateChat(chat.id, {
+        updatedAt: new Date(),
+        messages: {
+          create: newMessage,
+        },
+      });
+      // TODO: Make this a background job (or send to inference-service?)
+      // TODO: Save to Pinecone as vector
+      // Get vector from OpenAI
+      // Send vector to Pinecone
+      // const index = pinecone.Index("example-index");
+      // const upsertRequest = {
+      //   vectors: [
+      //     {
+      //       id: "vec1",
+      //       values: [0.1, 0.2, 0.3, 0.4],
+      //       metadata: {
+      //         genre: "drama",
+      //       },
+      //     },
+      //     {
+      //       id: "vec2",
+      //       values: [0.2, 0.3, 0.4, 0.5],
+      //       metadata: {
+      //         genre: "action",
+      //       },
+      //     },
+      //   ],
+      //   namespace: "example-namespace",
+      // };
+      // const upsertResponse = await index.upsert({ upsertRequest });
+      return newChat;
     } else {
       throw new InternalError("Could not get completion from OpenAI");
     }
