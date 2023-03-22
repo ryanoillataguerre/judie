36b0d456c59321f1c267d46dcaa65f69fb67e409
bcormier1 <bcormier@uci.edu>
2023-03-21 22:47:46 -0700

    wip

--- services/inference-service/server/inference_service_server.py
+++ services/inference-service/server/inference_service_server.py
@@ -1,4 +1,8 @@
 import inference_service_pb2
+import os
+import grpc
+from concurrent import futures
+
 
 class InferenceService(inference_service_pb2.InferenceServiceServicer):
     """
@@ -6,4 +10,20 @@ class InferenceService(inference_service_pb2.InferenceServiceServicer):
     """
 
     def GetChatResponse(self, request, context) -> None:
+
         raise NotImplementedError
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server.add_insecure_port(f':{os.getenv("PORT")}')
+
+    service.add_ProjectSvcServicer_to_server(api.API(), server)
+
+    server.start()
+    logging.info(f'Inference GRPC server running at on port {os.getenv("PORT")}')
+    server.wait_for_termination()
+
+
+if __name__ == '__main':
+    serve()
