f537509fdae9b62289c83923d073985a43cfb06e
bcormier1 <bcormier@uci.edu>
2023-03-25 18:08:22 -0700

    logging

--- services/inference-service/server/inference_service_server.py
+++ services/inference-service/server/inference_service_server.py
@@ -1,29 +1,49 @@
 import inference_service_pb2
+import inference_service_pb2_grpc
 import os
 import grpc
 from concurrent import futures
+import logging
+import sys
 
 
-class InferenceService(inference_service_pb2.InferenceServiceServicer):
+class InferenceServiceServicer(inference_service_pb2_grpc.InferenceServiceServicer):
     """
     Class to define the behavior of the Inference Service
     """
 
     def GetChatResponse(self, request, context) -> None:
-
-        raise NotImplementedError
+        print(request.Conversation.turn[-1])
 
 
 def serve():
+    grpc_port = os.getenv("GRPC_PORT")
+    logger.info(f'Attempting grpc connection on port: {grpc_port}',)
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    server.add_insecure_port(f':{os.getenv("PORT")}')
+    server.add_insecure_port(f'[::]:{grpc_port}')
 
-    service.add_ProjectSvcServicer_to_server(api.API(), server)
+    inference_service_pb2_grpc.add_InferenceServiceServicer_to_server(InferenceServiceServicer,
+                                                                      server)
 
     server.start()
-    logging.info(f'Inference GRPC server running at on port {os.getenv("PORT")}')
-    server.wait_for_termination()
+    logger.info(f'Inference GRPC server running at on port {grpc_port}')
+    server.wait_for_termination(timeout=3)
+    logger.info('Server ded')
 
+if __name__ == '__main__':
+    # set up logging
+    logger = logging.getLogger("inference_logger")
 
-if __name__ == '__main':
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(funcName)s - %(message)s')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    logger.info('Running serve()')
     serve()
