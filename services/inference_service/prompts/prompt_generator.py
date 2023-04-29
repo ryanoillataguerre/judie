ea598e8c96b2e7438126675eebcaa2e2028656d7
bcormier1 <bcormier@uci.edu>
2023-04-28 22:24:02 -0700

    initial prompts

--- services/inference_service/prompts/prompt_generator.py
+++ services/inference_service/prompts/prompt_generator.py
@@ -1,2 +1,10 @@
+from inference_service.prompts import prompt_chunks
+from inference_service.context.context_retriever import pull_context
 def generate_gpt_prompt(context, subject_modifier):
-    raise NotImplementedError
+    try:
+        subject_prompt = prompt_chunks.PROMPT_MAP[subject_modifier]
+    except KeyError:
+        subject_prompt = prompt_chunks.DEFAULT_PROMPT
+    context = pull_context()
+    subject_plus_context = subject_prompt + "\n Use the following context to help answer the next question. \n Context:\n" + context
+    
