6ef7b17f1961647a19685ef2f63b9bac6bb21b29
bcormier1 <bcormier@uci.edu>
2023-05-07 16:33:28 -0700

    format

--- services/inference_service/prompts/test_prompt_generator.py
+++ services/inference_service/prompts/test_prompt_generator.py
@@ -2,9 +2,11 @@ import pytest
 from inference_service.prompts.prompt_generator import generate_gpt_prompt
 from dotenv import load_dotenv
 
+
 @pytest.fixture
 def env_setup():
     load_dotenv()
+
 
 def test_prompt_generator_e2e(env_setup):
     full_prompt = generate_gpt_prompt(
