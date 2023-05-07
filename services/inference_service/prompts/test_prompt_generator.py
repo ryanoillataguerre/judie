05351466f2ad6d12629b83095b1838cdde64cc1a
bcormier1 <bcormier@uci.edu>
2023-05-07 16:33:05 -0700

    test update

--- services/inference_service/prompts/test_prompt_generator.py
+++ services/inference_service/prompts/test_prompt_generator.py
@@ -1,7 +1,12 @@
+import pytest
 from inference_service.prompts.prompt_generator import generate_gpt_prompt
+from dotenv import load_dotenv
 
+@pytest.fixture
+def env_setup():
+    load_dotenv()
 
-def test_prompt_generator_e2e():
+def test_prompt_generator_e2e(env_setup):
     full_prompt = generate_gpt_prompt(
         question="Teach me science", subject_modifier=None
     )
