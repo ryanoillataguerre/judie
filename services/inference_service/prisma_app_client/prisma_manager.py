6e89ab034934b8bb0b9f9f23d07249aa8e8ed8dd
bcormier1 <bcormier@uci.edu>
2023-05-19 10:16:06 -0700

    prisma manager

--- services/inference_service/prisma_app_client/prisma_manager.py
+++ services/inference_service/prisma_app_client/prisma_manager.py
@@ -1,1 +1,13 @@
-import prisma
+from typing import Optional
+from prisma import Prisma
+
+
+def get_chat(chat_id: str, app_db: Optional[Prisma] = None):
+    if not app_db:
+        app_db = Prisma()
+
+    await app_db.connect()
+    chats = await app_db.chat.find_many(where={
+        'id': chat_id,
+    },)
+    return chats
