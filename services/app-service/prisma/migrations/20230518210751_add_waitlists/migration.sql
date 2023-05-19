827c3f5cc0fdcca4de3afcfe64fe066739a81fc8
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-05-18 17:38:58 -0700

    Edit migration to not drop column

--- services/app-service/prisma/migrations/20230518210751_add_waitlists/migration.sql
+++ services/app-service/prisma/migrations/20230518210751_add_waitlists/migration.sql
@@ -1,15 +1,6 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `firstName` on the `users` table. All the data in the column will be lost.
-  - You are about to drop the column `lastName` on the `users` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "users" DROP COLUMN "firstName",
-DROP COLUMN "lastName",
-ADD COLUMN     "first_name" TEXT,
-ADD COLUMN     "last_name" TEXT;
+ALTER TABLE "users" RENAME COLUMN "firstName" TO "first_name";
+ALTER TABLE "users" RENAME COLUMN "lastName" TO "last_name";
 
 -- CreateTable
 CREATE TABLE "waitlist_entries" (
