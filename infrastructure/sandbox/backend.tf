d9b1d9aa29970f5b299d48d82b96064956ecb565
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-05-20 17:20:28 -0700

    Add comments

--- infrastructure/sandbox/backend.tf
+++ infrastructure/sandbox/backend.tf
@@ -1,3 +1,4 @@
+# https://cloud.google.com/docs/terraform/resource-management/store-state
 terraform {
  backend "gcs" {
    bucket  = "b09bcadeb5f255f1-bucket-tfstate"
--- infrastructure/sandbox/main.tf
+++ infrastructure/sandbox/main.tf
@@ -14,10 +14,10 @@ provider "google" {
 }
 
 # Store backend state in Cloud Storage
+# https://cloud.google.com/docs/terraform/resource-management/store-state
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
-
 resource "google_storage_bucket" "default" {
   name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
   force_destroy = false
