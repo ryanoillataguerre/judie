0465dc7377ca524bcc148f73fad8ce2a3f01ca82
bcormier1 <bcormier@uci.edu>
2023-05-28 00:29:06 -0700

    tests

--- services/inference_service/prisma_app_client/test_prisma_manger.py
+++ services/inference_service/prisma_app_client/test_prisma_manger.py
@@ -20,4 +20,7 @@ def test_get_chat_openai(env_setup):
     chats = prisma_manager.get_chat_openai_fmt(
         chat_id="52de7f7c-d4fe-4e33-91d6-a806ea1a3e42"
     )
+    assert len(chats) == 2
+    assert chats[0]["role"] == "user"
+    assert "Gregory XIII" in chats[1]["content"]
     print(chats)
