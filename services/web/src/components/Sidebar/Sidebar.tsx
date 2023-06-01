857d3654de0afc28e79057c7d0ae0800166a5faf
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-06-01 12:04:02 -0700

    Fix flashing buttons

--- services/web/src/components/Sidebar/Sidebar.tsx
+++ services/web/src/components/Sidebar/Sidebar.tsx
@@ -339,7 +339,7 @@ const Sidebar = ({ isOpen }: { isOpen: boolean }) => {
         key: "color-mode-switcher",
       },
     ];
-    if (!(auth?.userData?.subscription?.status === SubscriptionStatus.ACTIVE)) {
+    if (!(auth?.userData?.subscription?.status === SubscriptionStatus.ACTIVE) && !auth.loading && router.isReady && auth.userData) {
       options.push({
         key: "upgrade",
         icon: (
