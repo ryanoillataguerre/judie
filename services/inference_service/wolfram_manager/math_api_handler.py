bedf078056c2d16328076fa6aae551fe1b89c184
bcormier1 <bcormier@uci.edu>
2023-06-18 23:48:41 -0700

    wolfram api handler

--- services/inference_service/wolfram_manager/math_api_handler.py
+++ services/inference_service/wolfram_manager/math_api_handler.py
@@ -1,7 +1,10 @@
+import logging
 import os
-
 import urllib3
 from typing import Optional
+from lxml import etree
+
+logger = logging.getLogger("inference_logger")
 
 
 def pull_show_steps(
@@ -11,7 +14,26 @@ def pull_show_steps(
         http_pool = urllib3.PoolManager()
     response = http_pool.request(
         "GET",
-        f"http://api.wolframalpha.com/v2/query?appid={os.getenv('WOLFRAM_APP_ID')}&input={math_expresion}%3D11&podstate=Result__Step-by-step+solution&format=plaintext",
+        f"http://api.wolframalpha.com/v2/query?appid={os.getenv('WOLFRAM_APP_ID')}&input={math_expresion}&podstate=Result__Step-by-step+solution&format=plaintext",
     )
+    logger.debug("Response received from wolfram")
 
-    return response
+    tree = etree.fromstring(response.data)
+
+    # take text from primary response from primary evaluation type
+    try:
+        math_steps = tree.xpath("pod/subpod/plaintext")[0].text
+    except Error as e:
+        logger.info(f"Error parsing Wolfram steps response: {e}")
+        math_steps = ""
+
+    # grab additional context from primary evaluation
+    info = [
+        x.attrib["text"] for x in tree.xpath("pod/infos/info") if "text" in x.attrib
+    ]
+
+    math_context = math_steps
+    for i in info:
+        math_context += "\n" + i
+
+    return math_context
--- services/inference_service/wolfram_manager/test_math_api_handler.py
+++ services/inference_service/wolfram_manager/test_math_api_handler.py
@@ -3,5 +3,5 @@ from inference_service.wolfram_manager import math_api_handler
 
 
 def test_pull_show_steps(env_setup):
-    response = math_api_handler.pull_show_steps("Integral of ln(cos(x))")
+    response = math_api_handler.pull_show_steps("Integral of ln(x)cos(x)")
     print(response)
