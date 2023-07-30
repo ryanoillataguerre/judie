9a9c149e94385fd3ab049c5736424bc6e72e4b0b
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-07-30 11:07:30 -0700

    gRPC in app-service (#112)
    * Stashing progress - so close...
    
    (cherry picked from commit 09d09cde5c866e9d86d15903804d4a16576a37fa)
    
    * Stable... holy hell
    
    (cherry picked from commit 1d7348a205bb10d4e3457a22cd4484a6a50c0199)
    
    * Streaming with gRPC working - need to save returned message
    
    (cherry picked from commit 71f10bf3e16754ece1df2046697e2a0e46956d12)
    
    * Add comment
    
    (cherry picked from commit 7d006cd87c40bc043f597dceedc23c70554faa02)
    
    * Update chat and inference services to follow DDD
    
    * Clean up

--- services/app-service/src/inference/service.ts
+++ services/app-service/src/inference/service.ts
@@ -1,1 +1,29 @@
+import { Response } from "express";
 import inferenceServiceClient from "../utils/grpc/inferenceClient.js";
+import { ChatDetails } from "../proto/inference_service.js";
+
+export const getChatCompletion = async ({
+  chatId,
+  response,
+}: {
+  chatId: string;
+  response: Response;
+}) => {
+  const chatRequest: ChatDetails = {
+    chatId,
+  };
+  const result = inferenceServiceClient.getChatResponse(chatRequest);
+  const fullResponse = [];
+  for await (const chunk of result) {
+    if (chunk.responsePart) {
+      fullResponse.push(chunk.responsePart);
+      response.write(chunk.responsePart);
+    }
+    // Else do nothing (for now)
+  }
+  const fullText = fullResponse.join("");
+
+  // TODO: Create verbose response here for the web to consume
+  // Add flags, notices, quizzes, etc.
+  return fullText;
+};
