31b78aa6eb70a0732ad329a3e270f96fa05f1709
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-08-04 21:59:39 -0700

    Loading indicator on chat (#117)
    * Add loading indicator - almost there. Streaming issue has put a hold on it
    
    * Remove css
    
    * Remove sass
    
    * Stashing changs
    
    * Animated ellipsis is working

--- services/web/src/components/Chat/Chat.tsx
+++ services/web/src/components/Chat/Chat.tsx
@@ -1,6 +1,14 @@
-import { memo, use, useContext, useEffect, useMemo, useRef } from "react";
+import {
+  memo,
+  use,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Flex, Text, VStack, useBreakpointValue } from "@chakra-ui/react";
-import  { ChatContext, UIMessageType } from "@judie/hooks/useChat";
+import { ChatContext, UIMessageType } from "@judie/hooks/useChat";
 import SubjectSelector from "../SubjectSelector/SubjectSelector";
 import MessageRow from "../MessageRow/MessageRow";
 import { MessageType } from "@judie/data/types/api";
@@ -8,27 +16,33 @@ import ScrollContainer from "../ScrollContainer/ScrollContainer";
 import Paywall from "../Paywall/Paywall";
 import { useRouter } from "next/router";
 
-// const MessageRowMemo = memo(MessageRow, (prevProps, nextProps) => prevProps.message.readableContent === nextProps.message.readableContent);
-
-const Chat = ({
-  initialQuery,
-}: {
-  initialQuery?: string;
-}) => {
-  const { tempUserMessageChatId, beingStreamedChatId, chat, streaming, submitSubject, messages, beingStreamedMessage, tempUserMessage, setTempUserMessage, paywallOpen, setPaywallOpen } = useContext(ChatContext);
+const Chat = ({ initialQuery }: { initialQuery?: string }) => {
+  const {
+    tempUserMessageChatId,
+    beingStreamedChatId,
+    chat,
+    streaming,
+    submitSubject,
+    messages,
+    beingStreamedMessage,
+    tempUserMessage,
+    setTempUserMessage,
+    paywallOpen,
+    setPaywallOpen,
+  } = useContext(ChatContext);
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   const router = useRouter();
   const chatId = router.query.id;
 
   const scroll = () => {
-    const offsetHeight = scrollContainerRef.current?.offsetHeight || 0
-    const scrollHeight = scrollContainerRef.current?.scrollHeight || 0
-    const scrollTop = scrollContainerRef.current?.scrollTop || 0
+    const offsetHeight = scrollContainerRef.current?.offsetHeight || 0;
+    const scrollHeight = scrollContainerRef.current?.scrollHeight || 0;
+    const scrollTop = scrollContainerRef.current?.scrollTop || 0;
     if (scrollHeight <= scrollTop + offsetHeight + 100) {
-      scrollContainerRef.current?.scrollTo(0, scrollHeight)
+      scrollContainerRef.current?.scrollTo(0, scrollHeight);
     }
-  }
+  };
   const subjectSelectorWidth = useBreakpointValue({
     base: "80%",
     md: "50%",
@@ -38,23 +52,25 @@ const Chat = ({
     setTempUserMessage(undefined);
   }, [setTempUserMessage]);
   useEffect(() => {
-    scroll()
-  }, [messages, tempUserMessage, beingStreamedMessage])
+    scroll();
+  }, [messages, tempUserMessage, beingStreamedMessage]);
 
   const renderedMessages = useMemo(() => {
     let newMessages: UIMessageType[] = messages;
     if (streaming) {
-      if (tempUserMessage && (tempUserMessageChatId === chatId)) {
-        newMessages = [...newMessages, tempUserMessage]
+      if (tempUserMessage && tempUserMessageChatId === chatId) {
+        newMessages = [...newMessages, tempUserMessage];
       }
     }
     return newMessages.map((message, index) => {
-      const isLast = ((index + 1) === messages.length);
-      const key = `${message.type}-${isLast && message.type === MessageType.BOT ? `mostRecent` : message.readableContent?.slice(0, 9).includes("undefined") ? message.readableContent?.slice(9, 50) : message.readableContent?.slice(0, 50)}`;
-      return (
-        <MessageRow key={key} message={message} />
-      )
-    })
+      const isLast = index + 1 === messages.length;
+      const key = `${message.type}-${
+        message.readableContent?.slice(0, 9).includes("undefined")
+          ? message.readableContent?.slice(9, 50)
+          : message.readableContent?.slice(0, 50)
+      }`;
+      return <MessageRow key={key} message={message} />;
+    });
   }, [messages, tempUserMessage, streaming, chatId, tempUserMessageChatId]);
 
   const showSubjectSelector: boolean = useMemo(() => {
@@ -66,7 +82,23 @@ const Chat = ({
       shouldShowSubjectSelector = false;
     }
     return shouldShowSubjectSelector;
-  }, [chat?.messages?.length, tempUserMessage, chatId, tempUserMessageChatId])
+  }, [chat?.messages?.length, tempUserMessage, chatId, tempUserMessageChatId]);
+
+  const [animatedEllipsisStringValue, setAnimatedEllipsisStringValue] =
+    useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAnimatedEllipsisStringValue((prev) => {
+        if (prev.length === 3) {
+          return ".";
+        } else {
+          return prev + ".";
+        }
+      });
+    }, 400);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Flex
@@ -78,60 +110,77 @@ const Chat = ({
         scrollPadding: "10rem",
       }}
     >
-      <Paywall isOpen={paywallOpen ?? false} setIsOpen={setPaywallOpen}  />
+      <Paywall isOpen={paywallOpen ?? false} setIsOpen={setPaywallOpen} />
       {showSubjectSelector ? (
-        <VStack style={{
+        <VStack
+          style={{
             width: subjectSelectorWidth,
             padding: "2rem",
             border: "#565555 0.5px solid",
             borderRadius: "0.8rem",
-        }} boxShadow={"sm"}>
+          }}
+          boxShadow={"sm"}
+        >
           {!chat?.subject ? (
-                <Flex width={"100%"}>
-                  <Text
-                    style={{
-                      fontSize: "1.2rem",
-                      fontWeight: 600,
-                      marginBottom: "1rem",
-                    }}
-                  >
-                      What would you like to chat about?
-                    </Text>
-                  </Flex>
-            ) : (
-              <Flex width={"100%"}>
-                <Text
-                  style={{
-                    fontSize: "0.8rem",
-                    fontWeight: 400,
-                    marginBottom: "0.5rem",
-                  }}
-                >
-                  You can change your subject until you send your first message
-                </Text>
-              </Flex>
-            )}
-            <SubjectSelector
-              width={"100%"}
-              selectSubject={submitSubject}
-            />
-      </VStack>
-      ) : (
-          <ScrollContainer>
-            {renderedMessages}
-            {beingStreamedMessage && (streaming || (beingStreamedChatId === chatId)) && (
-              <MessageRow
-                key={`${MessageType.BOT}-mostRecent`}
-                message={{
-                  type: MessageType.BOT,
-                  readableContent: beingStreamedMessage.slice(9, -1),
-                  createdAt: new Date(),
+            <Flex width={"100%"}>
+              <Text
+                style={{
+                  fontSize: "1.2rem",
+                  fontWeight: 600,
+                  marginBottom: "1rem",
                 }}
-              />
-            )}
-          </ScrollContainer>
-        )
-      }
+              >
+                What would you like to chat about?
+              </Text>
+            </Flex>
+          ) : (
+            <Flex width={"100%"}>
+              <Text
+                style={{
+                  fontSize: "0.8rem",
+                  fontWeight: 400,
+                  marginBottom: "0.5rem",
+                }}
+              >
+                You can change your subject until you send your first message
+              </Text>
+            </Flex>
+          )}
+          <SubjectSelector width={"100%"} selectSubject={submitSubject} />
+        </VStack>
+      ) : (
+        // <ScrollContainer>
+        //     {renderedMessages}
+        //     {beingStreamedMessage && (streaming || (beingStreamedChatId === chatId)) && (
+        //       <MessageRow
+        //         key={`${MessageType.BOT}-mostRecent`}
+        //         message={{
+        //           type: MessageType.BOT,
+        //           readableContent: beingStreamedMessage.slice(9, -1),
+        //           createdAt: new Date(),
+        //         }}
+        //       />
+        //     )}
+        //   </ScrollContainer>
+        <ScrollContainer>
+          {renderedMessages}
+          {(streaming ||
+            beingStreamedChatId === chatId ||
+            beingStreamedMessage) && (
+            <MessageRow
+              key={`${MessageType.BOT}-mostRecent`}
+              message={{
+                type: MessageType.BOT,
+                readableContent:
+                  beingStreamedMessage?.slice(9, -1) ||
+                  animatedEllipsisStringValue,
+                createdAt: new Date(),
+              }}
+              // isStreaming={streaming}
+            />
+          )}
+        </ScrollContainer>
+      )}
     </Flex>
   );
 };
