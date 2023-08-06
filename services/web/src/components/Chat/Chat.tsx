bf9d4c3f77f5ad29f0f2b870db3117cab3c66717
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-08-06 13:18:47 -0700

    Update spinner conditions on Chat

--- services/web/src/components/Chat/Chat.tsx
+++ services/web/src/components/Chat/Chat.tsx
@@ -22,6 +22,8 @@ import ScrollContainer from "../ScrollContainer/ScrollContainer";
 import Paywall from "../Paywall/Paywall";
 import { useRouter } from "next/router";
 import Loading from "../lottie/Loading/Loading";
+import { GET_CHAT_BY_ID, getChatByIdQuery } from "@judie/data/queries";
+import { useQuery } from "react-query";
 
 const Chat = ({ initialQuery }: { initialQuery?: string }) => {
   const {
@@ -36,6 +38,7 @@ const Chat = ({ initialQuery }: { initialQuery?: string }) => {
     setTempUserMessage,
     paywallOpen,
     setPaywallOpen,
+    displayWelcome,
   } = useContext(ChatContext);
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
@@ -79,16 +82,23 @@ const Chat = ({ initialQuery }: { initialQuery?: string }) => {
     });
   }, [messages, tempUserMessage, streaming, chatId, tempUserMessageChatId]);
 
-  const showSubjectSelector: boolean = useMemo(() => {
-    let shouldShowSubjectSelector: boolean = true;
-    if (chat?.messages?.length) {
-      shouldShowSubjectSelector = false;
-    }
-    if (tempUserMessage && tempUserMessageChatId === chatId) {
-      shouldShowSubjectSelector = false;
-    }
-    return shouldShowSubjectSelector;
-  }, [chat?.messages?.length, tempUserMessage, chatId, tempUserMessageChatId]);
+  const existingChatQuery = useQuery({
+    queryKey: [GET_CHAT_BY_ID, chatId],
+    enabled: !!chatId,
+    refetchOnWindowFocus: false,
+    queryFn: () => getChatByIdQuery(chatId as string),
+  });
+
+  // const showSubjectSelector: boolean = useMemo(() => {
+  //   let shouldShowSubjectSelector: boolean = true;
+  //   if (chat?.messages?.length) {
+  //     shouldShowSubjectSelector = false;
+  //   }
+  //   if (tempUserMessage && tempUserMessageChatId === chatId) {
+  //     shouldShowSubjectSelector = false;
+  //   }
+  //   return shouldShowSubjectSelector;
+  // }, [chat?.messages?.length, tempUserMessage, chatId, tempUserMessageChatId]);
 
   const [animatedEllipsisStringValue, setAnimatedEllipsisStringValue] =
     useState("");
@@ -117,45 +127,47 @@ const Chat = ({ initialQuery }: { initialQuery?: string }) => {
       }}
     >
       <Paywall isOpen={paywallOpen ?? false} setIsOpen={setPaywallOpen} />
-      {!chat || (chat && !chat.subject && !renderedMessages?.length) ? (
-        <Spinner size="xl" colorScheme="teal" />
-      ) : showSubjectSelector ? (
-        <VStack
-          style={{
-            width: subjectSelectorWidth,
-            padding: "2rem",
-            border: "#565555 0.5px solid",
-            borderRadius: "0.8rem",
-          }}
-          boxShadow={"sm"}
-        >
-          {!chat?.subject ? (
-            <Flex width={"100%"}>
-              <Text
-                style={{
-                  fontSize: "1.2rem",
-                  fontWeight: 600,
-                  marginBottom: "1rem",
-                }}
-              >
-                What would you like to chat about?
-              </Text>
-            </Flex>
-          ) : (
-            <Flex width={"100%"}>
-              <Text
-                style={{
-                  fontSize: "0.8rem",
-                  fontWeight: 400,
-                  marginBottom: "0.5rem",
-                }}
-              >
-                You can change your subject until you send your first message
-              </Text>
-            </Flex>
-          )}
-          <SubjectSelector width={"100%"} selectSubject={submitSubject} />
-        </VStack>
+      {displayWelcome ? (
+        existingChatQuery?.isLoading ? (
+          <Spinner color={"blue.300"} size={"lg"} />
+        ) : (
+          <VStack
+            style={{
+              width: subjectSelectorWidth,
+              padding: "2rem",
+              border: "#565555 0.5px solid",
+              borderRadius: "0.8rem",
+            }}
+            boxShadow={"sm"}
+          >
+            {!chat?.subject ? (
+              <Flex width={"100%"}>
+                <Text
+                  style={{
+                    fontSize: "1.2rem",
+                    fontWeight: 600,
+                    marginBottom: "1rem",
+                  }}
+                >
+                  What would you like to chat about?
+                </Text>
+              </Flex>
+            ) : (
+              <Flex width={"100%"}>
+                <Text
+                  style={{
+                    fontSize: "0.8rem",
+                    fontWeight: 400,
+                    marginBottom: "0.5rem",
+                  }}
+                >
+                  You can change your subject until you send your first message
+                </Text>
+              </Flex>
+            )}
+            <SubjectSelector width={"100%"} selectSubject={submitSubject} />
+          </VStack>
+        )
       ) : (
         <ScrollContainer>
           {renderedMessages}
