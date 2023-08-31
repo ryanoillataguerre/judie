fff6e15e49d2d898a9692f48d6d507ba4ee342ef
samotleriche <samotleriche@gmail.com>
2023-09-01 00:40:45 +0200

    Fixing Infinite render loop in SidebarChatItem

--- services/web/src/components/SidebarChatItem/SidebarChatItem.tsx
+++ services/web/src/components/SidebarChatItem/SidebarChatItem.tsx
@@ -1,32 +1,16 @@
 import React, { useState, useMemo, useEffect } from "react";
 import {
-  Box,
   Button,
-  Divider,
   Editable,
   EditableInput,
   EditablePreview,
   Flex,
   IconButton,
-  Image,
   Input,
-  Spinner,
-  Stack,
-  Text,
-  Modal,
-  ModalBody,
-  ModalOverlay,
-  ModalContent,
-  useColorModeValue,
   useEditableControls,
-  useToast,
-  useBreakpointValue,
+  useColorModeValue,
 } from "@chakra-ui/react";
-import {
-  Chat,
-  PermissionType,
-  SubscriptionStatus,
-} from "@judie/data/types/api";
+import { Chat } from "@judie/data/types/api";
 import { useRouter } from "next/router";
 import { useMutation } from "react-query";
 import { putChatMutation } from "@judie/data/mutations";
@@ -37,6 +21,85 @@ import { getTitleForChat } from "@judie/utils/chat/getTitleForChat";
 import FileIcon from "@judie/components/icons/FileIcon";
 import TrashIcon from "@judie/components/icons/TrashIcon";
 import EditIcon from "@judie/components/icons/EditIcon";
+
+type EditableControlsProps = {
+  chat: Chat;
+  isSelected: boolean;
+  setBeingEditedChatId: (chatId: string | null) => void;
+  setBeingDeletedChatId: (chatId: string) => void;
+};
+
+const EditableControls = ({
+  chat,
+  isSelected,
+  setBeingEditedChatId,
+  setBeingDeletedChatId,
+}: EditableControlsProps) => {
+  const iconColor = useColorModeValue("#333", "#FFF");
+
+  const {
+    isEditing,
+    getSubmitButtonProps,
+    getCancelButtonProps,
+    getEditButtonProps,
+  } = useEditableControls();
+  // No other good way to set the being edited chat ID other than this unfortunately
+  // onClicks are reserved for submitbuttonprops etc.
+  useEffect(() => {
+    if (isEditing) {
+      setBeingEditedChatId(chat.id);
+    }
+  }, [isEditing]);
+
+  return (
+    <>
+      {isSelected && (
+        <Flex
+          style={{
+            flexDirection: "row",
+            // height: "90%",
+          }}
+          ml={"10px"}
+          gap={"10px"}
+        >
+          <IconButton
+            aria-label="Edit Chat Title"
+            variant="ghost"
+            size="xs"
+            zIndex={100}
+            // onClick={isEditing ? null : () => setIsBeingEditedChatId(chat.id)}
+            {...(isEditing ? getSubmitButtonProps() : getEditButtonProps())}
+            icon={
+              isEditing ? (
+                <AiOutlineCheck size={18} color={"#A3A3A3"} />
+              ) : (
+                <EditIcon boxSize={6} color={iconColor} />
+              )
+            }
+          />
+          <IconButton
+            aria-label="Delete Chat"
+            variant="ghost"
+            size="xs"
+            zIndex={100}
+            {...(isEditing
+              ? getCancelButtonProps()
+              : {
+                  onClick: () => setBeingDeletedChatId(chat.id),
+                })}
+            icon={
+              isEditing ? (
+                <RxCross2 size={18} color={"#A3A3A3"} />
+              ) : (
+                <TrashIcon boxSize={6} color={iconColor} />
+              )
+            }
+          />
+        </Flex>
+      )}
+    </>
+  );
+};
 
 const SidebarChatItem = ({
   chat,
@@ -50,6 +113,8 @@ const SidebarChatItem = ({
   beingEditedChatId?: string | null;
 }) => {
   const router = useRouter();
+  const iconColor = useColorModeValue("#333", "#FFF");
+  const itemHoverColor = useColorModeValue("rgba(60, 20, 120, 0.05)", "#333");
 
   const [editingValue, setEditingValue] = useState<string>();
 
@@ -71,72 +136,6 @@ const SidebarChatItem = ({
 
   // const isEditing = beingEditedChatId === chat.id;
 
-  const EditableControls = () => {
-    const {
-      isEditing,
-      getSubmitButtonProps,
-      getCancelButtonProps,
-      getEditButtonProps,
-    } = useEditableControls();
-    // No other good way to set the being edited chat ID other than this unfortunately
-    // onClicks are reserved for submitbuttonprops etc.
-    useEffect(() => {
-      if (isEditing) {
-        setBeingEditedChatId(chat.id);
-      }
-    }, [isEditing]);
-    return (
-      <>
-        {isSelected ? (
-          <Flex
-            style={{
-              flexDirection: "row",
-              height: "90%",
-            }}
-            ml={"10px"}
-            gap={"10px"}
-          >
-            <IconButton
-              aria-label="Edit Chat Title"
-              variant="ghost"
-              size="xs"
-              zIndex={100}
-              // onClick={isEditing ? null : () => setIsBeingEditedChatId(chat.id)}
-              {...(isEditing ? getSubmitButtonProps() : getEditButtonProps())}
-              icon={
-                isEditing ? (
-                  <AiOutlineCheck size={18} color={"#A3A3A3"} />
-                ) : (
-                  <EditIcon boxSize={6} color="#A3A3A3" />
-                )
-              }
-            />
-            <IconButton
-              aria-label="Delete Chat"
-              variant="ghost"
-              size="xs"
-              zIndex={100}
-              {...(isEditing
-                ? getCancelButtonProps()
-                : {
-                    onClick: () => setBeingDeletedChatId(chat.id),
-                  })}
-              icon={
-                isEditing ? (
-                  <RxCross2 size={18} color={"#A3A3A3"} />
-                ) : (
-                  <TrashIcon boxSize={6} color="#A3A3A3" />
-                )
-              }
-            />
-          </Flex>
-        ) : (
-          <></>
-        )}
-      </>
-    );
-  };
-
   return (
     <Button
       variant={"ghost"}
@@ -146,15 +145,15 @@ const SidebarChatItem = ({
       px={"20px"}
       borderRadius={"11px"}
       _hover={{
-        backgroundColor: "rgba(60, 20, 120, 0.05);",
+        backgroundColor: itemHoverColor,
       }}
-      bg={isSelected ? "rgba(60, 20, 120, 0.05);" : "transparent"}
+      bg={isSelected ? itemHoverColor : "transparent"}
       display={"flex"}
       gap={"10px"}
       maxW={"279px"}
       h={"auto"}
     >
-      <FileIcon boxSize={6} />
+      <FileIcon boxSize={6} color={iconColor} />
       <Editable
         onClick={() => {
           if (!editingValue) {
@@ -214,7 +213,12 @@ const SidebarChatItem = ({
             <EditablePreview p={0} w={"fit-content"} cursor="pointer" />
             <Input h={"unset"} p={0} textAlign={"start"} as={EditableInput} />
           </Flex>
-          <EditableControls />
+          <EditableControls
+            chat={chat}
+            setBeingDeletedChatId={setBeingDeletedChatId}
+            setBeingEditedChatId={setBeingEditedChatId}
+            isSelected={isSelected}
+          />
         </Flex>
       </Editable>
     </Button>
