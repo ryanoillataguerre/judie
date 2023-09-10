c4063f17d3ac1237b3b9f87d84e4a160fd04f295
samotleriche <samotleriche@gmail.com>
2023-09-10 09:58:18 -0700

    adding search bar component to tables

---
+++ services/web/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { Search2Icon } from "@chakra-ui/icons";
+import { useToast } from "@chakra-ui/react";
+
+type SearchBarProps = {
+  title: string;
+  searchText: string;
+  setSearchText: (searchText: string) => void;
+};
+
+// submit on press enter
+// clear on esc
+
+const SearchBar = ({ title, searchText, setSearchText }: SearchBarProps) => {
+  const toast = useToast();
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      toast({
+        title: "Table Updated",
+        status: "info",
+        duration: 1000,
+        isClosable: true,
+      });
+    } else if (e.key === "Escape") {
+      setSearchText("");
+    }
+  };
+
+  return (
+    <Flex flexGrow={1} alignItems={"center"} mb={4}>
+      <InputGroup>
+        <InputLeftElement
+          py={15}
+          h={54}
+          pl={4}
+          pointerEvents="none"
+          children={<Search2Icon color="gray.300" />}
+        />
+        <Input
+          borderRadius={"100px"}
+          py={15}
+          pl={45}
+          h={54}
+          type="text"
+          placeholder={`Search ${title}`}
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+          onKeyDown={onKeyDown}
+        />
+      </InputGroup>
+    </Flex>
+  );
+};
+
+export default SearchBar;
