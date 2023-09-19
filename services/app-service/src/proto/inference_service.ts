735c6576c915c3cd1d56c8a3b95779c4c1448f07
bcormier1 <bcormier@uci.edu>
2023-09-18 17:59:53 -0700

    camelCase rpc method names

--- services/app-service/src/proto/inference_service.ts
+++ services/app-service/src/proto/inference_service.ts
@@ -44,10 +44,7 @@ function createBaseChatDetails(): ChatDetails {
 }
 
 export const ChatDetails = {
-  encode(
-    message: ChatDetails,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
+  encode(message: ChatDetails, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.chatId !== "") {
       writer.uint32(10).string(message.chatId);
     }
@@ -55,8 +52,7 @@ export const ChatDetails = {
   },
 
   decode(input: _m0.Reader | Uint8Array, length?: number): ChatDetails {
-    const reader =
-      input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = createBaseChatDetails();
     while (reader.pos < end) {
@@ -93,9 +89,7 @@ export const ChatDetails = {
   create<I extends Exact<DeepPartial<ChatDetails>, I>>(base?: I): ChatDetails {
     return ChatDetails.fromPartial(base ?? ({} as any));
   },
-  fromPartial<I extends Exact<DeepPartial<ChatDetails>, I>>(
-    object: I
-  ): ChatDetails {
+  fromPartial<I extends Exact<DeepPartial<ChatDetails>, I>>(object: I): ChatDetails {
     const message = createBaseChatDetails();
     message.chatId = object.chatId ?? "";
     return message;
@@ -107,25 +101,18 @@ function createBaseTutorResponse(): TutorResponse {
 }
 
 export const TutorResponse = {
-  encode(
-    message: TutorResponse,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
+  encode(message: TutorResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.responsePart !== "") {
       writer.uint32(10).string(message.responsePart);
     }
     Object.entries(message.chatMetaData).forEach(([key, value]) => {
-      TutorResponse_ChatMetaDataEntry.encode(
-        { key: key as any, value },
-        writer.uint32(18).fork()
-      ).ldelim();
+      TutorResponse_ChatMetaDataEntry.encode({ key: key as any, value }, writer.uint32(18).fork()).ldelim();
     });
     return writer;
   },
 
   decode(input: _m0.Reader | Uint8Array, length?: number): TutorResponse {
-    const reader =
-      input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = createBaseTutorResponse();
     while (reader.pos < end) {
@@ -143,10 +130,7 @@ export const TutorResponse = {
             break;
           }
 
-          const entry2 = TutorResponse_ChatMetaDataEntry.decode(
-            reader,
-            reader.uint32()
-          );
+          const entry2 = TutorResponse_ChatMetaDataEntry.decode(reader, reader.uint32());
           if (entry2.value !== undefined) {
             message.chatMetaData[entry2.key] = entry2.value;
           }
@@ -162,17 +146,12 @@ export const TutorResponse = {
 
   fromJSON(object: any): TutorResponse {
     return {
-      responsePart: isSet(object.responsePart)
-        ? String(object.responsePart)
-        : "",
+      responsePart: isSet(object.responsePart) ? String(object.responsePart) : "",
       chatMetaData: isObject(object.chatMetaData)
-        ? Object.entries(object.chatMetaData).reduce<{ [key: string]: string }>(
-            (acc, [key, value]) => {
-              acc[key] = String(value);
-              return acc;
-            },
-            {}
-          )
+        ? Object.entries(object.chatMetaData).reduce<{ [key: string]: string }>((acc, [key, value]) => {
+          acc[key] = String(value);
+          return acc;
+        }, {})
         : {},
     };
   },
@@ -194,24 +173,21 @@ export const TutorResponse = {
     return obj;
   },
 
-  create<I extends Exact<DeepPartial<TutorResponse>, I>>(
-    base?: I
-  ): TutorResponse {
+  create<I extends Exact<DeepPartial<TutorResponse>, I>>(base?: I): TutorResponse {
     return TutorResponse.fromPartial(base ?? ({} as any));
   },
-  fromPartial<I extends Exact<DeepPartial<TutorResponse>, I>>(
-    object: I
-  ): TutorResponse {
+  fromPartial<I extends Exact<DeepPartial<TutorResponse>, I>>(object: I): TutorResponse {
     const message = createBaseTutorResponse();
     message.responsePart = object.responsePart ?? "";
-    message.chatMetaData = Object.entries(object.chatMetaData ?? {}).reduce<{
-      [key: string]: string;
-    }>((acc, [key, value]) => {
-      if (value !== undefined) {
-        acc[key] = String(value);
-      }
-      return acc;
-    }, {});
+    message.chatMetaData = Object.entries(object.chatMetaData ?? {}).reduce<{ [key: string]: string }>(
+      (acc, [key, value]) => {
+        if (value !== undefined) {
+          acc[key] = String(value);
+        }
+        return acc;
+      },
+      {},
+    );
     return message;
   },
 };
@@ -221,10 +197,7 @@ function createBaseTutorResponse_ChatMetaDataEntry(): TutorResponse_ChatMetaData
 }
 
 export const TutorResponse_ChatMetaDataEntry = {
-  encode(
-    message: TutorResponse_ChatMetaDataEntry,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
+  encode(message: TutorResponse_ChatMetaDataEntry, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.key !== "") {
       writer.uint32(10).string(message.key);
     }
@@ -234,12 +207,8 @@ export const TutorResponse_ChatMetaDataEntry = {
     return writer;
   },
 
-  decode(
-    input: _m0.Reader | Uint8Array,
-    length?: number
-  ): TutorResponse_ChatMetaDataEntry {
-    const reader =
-      input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+  decode(input: _m0.Reader | Uint8Array, length?: number): TutorResponse_ChatMetaDataEntry {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = createBaseTutorResponse_ChatMetaDataEntry();
     while (reader.pos < end) {
@@ -269,10 +238,7 @@ export const TutorResponse_ChatMetaDataEntry = {
   },
 
   fromJSON(object: any): TutorResponse_ChatMetaDataEntry {
-    return {
-      key: isSet(object.key) ? String(object.key) : "",
-      value: isSet(object.value) ? String(object.value) : "",
-    };
+    return { key: isSet(object.key) ? String(object.key) : "", value: isSet(object.value) ? String(object.value) : "" };
   },
 
   toJSON(message: TutorResponse_ChatMetaDataEntry): unknown {
@@ -286,13 +252,11 @@ export const TutorResponse_ChatMetaDataEntry = {
     return obj;
   },
 
-  create<I extends Exact<DeepPartial<TutorResponse_ChatMetaDataEntry>, I>>(
-    base?: I
-  ): TutorResponse_ChatMetaDataEntry {
+  create<I extends Exact<DeepPartial<TutorResponse_ChatMetaDataEntry>, I>>(base?: I): TutorResponse_ChatMetaDataEntry {
     return TutorResponse_ChatMetaDataEntry.fromPartial(base ?? ({} as any));
   },
   fromPartial<I extends Exact<DeepPartial<TutorResponse_ChatMetaDataEntry>, I>>(
-    object: I
+    object: I,
   ): TutorResponse_ChatMetaDataEntry {
     const message = createBaseTutorResponse_ChatMetaDataEntry();
     message.key = object.key ?? "";
@@ -306,22 +270,15 @@ function createBaseReturnConnectedCheck(): ReturnConnectedCheck {
 }
 
 export const ReturnConnectedCheck = {
-  encode(
-    message: ReturnConnectedCheck,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
+  encode(message: ReturnConnectedCheck, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.returnCheck === true) {
       writer.uint32(8).bool(message.returnCheck);
     }
     return writer;
   },
 
-  decode(
-    input: _m0.Reader | Uint8Array,
-    length?: number
-  ): ReturnConnectedCheck {
-    const reader =
-      input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+  decode(input: _m0.Reader | Uint8Array, length?: number): ReturnConnectedCheck {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = createBaseReturnConnectedCheck();
     while (reader.pos < end) {
@@ -344,11 +301,7 @@ export const ReturnConnectedCheck = {
   },
 
   fromJSON(object: any): ReturnConnectedCheck {
-    return {
-      returnCheck: isSet(object.returnCheck)
-        ? Boolean(object.returnCheck)
-        : false,
-    };
+    return { returnCheck: isSet(object.returnCheck) ? Boolean(object.returnCheck) : false };
   },
 
   toJSON(message: ReturnConnectedCheck): unknown {
@@ -359,14 +312,10 @@ export const ReturnConnectedCheck = {
     return obj;
   },
 
-  create<I extends Exact<DeepPartial<ReturnConnectedCheck>, I>>(
-    base?: I
-  ): ReturnConnectedCheck {
+  create<I extends Exact<DeepPartial<ReturnConnectedCheck>, I>>(base?: I): ReturnConnectedCheck {
     return ReturnConnectedCheck.fromPartial(base ?? ({} as any));
   },
-  fromPartial<I extends Exact<DeepPartial<ReturnConnectedCheck>, I>>(
-    object: I
-  ): ReturnConnectedCheck {
+  fromPartial<I extends Exact<DeepPartial<ReturnConnectedCheck>, I>>(object: I): ReturnConnectedCheck {
     const message = createBaseReturnConnectedCheck();
     message.returnCheck = object.returnCheck ?? false;
     return message;
@@ -378,22 +327,15 @@ function createBaseConnectedCheckResponse(): ConnectedCheckResponse {
 }
 
 export const ConnectedCheckResponse = {
-  encode(
-    message: ConnectedCheckResponse,
-    writer: _m0.Writer = _m0.Writer.create()
-  ): _m0.Writer {
+  encode(message: ConnectedCheckResponse, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
     if (message.connected === true) {
       writer.uint32(8).bool(message.connected);
     }
     return writer;
   },
 
-  decode(
-    input: _m0.Reader | Uint8Array,
-    length?: number
-  ): ConnectedCheckResponse {
-    const reader =
-      input instanceof _m0.Reader ? input : _m0.Reader.create(input);
+  decode(input: _m0.Reader | Uint8Array, length?: number): ConnectedCheckResponse {
+    const reader = input instanceof _m0.Reader ? input : _m0.Reader.create(input);
     let end = length === undefined ? reader.len : reader.pos + length;
     const message = createBaseConnectedCheckResponse();
     while (reader.pos < end) {
@@ -416,9 +358,7 @@ export const ConnectedCheckResponse = {
   },
 
   fromJSON(object: any): ConnectedCheckResponse {
-    return {
-      connected: isSet(object.connected) ? Boolean(object.connected) : false,
-    };
+    return { connected: isSet(object.connected) ? Boolean(object.connected) : false };
   },
 
   toJSON(message: ConnectedCheckResponse): unknown {
@@ -429,14 +369,10 @@ export const ConnectedCheckResponse = {
     return obj;
   },
 
-  create<I extends Exact<DeepPartial<ConnectedCheckResponse>, I>>(
-    base?: I
-  ): ConnectedCheckResponse {
+  create<I extends Exact<DeepPartial<ConnectedCheckResponse>, I>>(base?: I): ConnectedCheckResponse {
     return ConnectedCheckResponse.fromPartial(base ?? ({} as any));
   },
-  fromPartial<I extends Exact<DeepPartial<ConnectedCheckResponse>, I>>(
-    object: I
-  ): ConnectedCheckResponse {
+  fromPartial<I extends Exact<DeepPartial<ConnectedCheckResponse>, I>>(object: I): ConnectedCheckResponse {
     const message = createBaseConnectedCheckResponse();
     message.connected = object.connected ?? false;
     return message;
@@ -446,111 +382,72 @@ export const ConnectedCheckResponse = {
 export type InferenceServiceService = typeof InferenceServiceService;
 export const InferenceServiceService = {
   getChatResponse: {
-    path: "/inferenceServiceServer.InferenceService/GetChatResponse",
+    path: "/inferenceServiceServer.InferenceService/getChatResponse",
     requestStream: false,
     responseStream: true,
-    requestSerialize: (value: ChatDetails) =>
-      Buffer.from(ChatDetails.encode(value).finish()),
+    requestSerialize: (value: ChatDetails) => Buffer.from(ChatDetails.encode(value).finish()),
     requestDeserialize: (value: Buffer) => ChatDetails.decode(value),
-    responseSerialize: (value: TutorResponse) =>
-      Buffer.from(TutorResponse.encode(value).finish()),
+    responseSerialize: (value: TutorResponse) => Buffer.from(TutorResponse.encode(value).finish()),
     responseDeserialize: (value: Buffer) => TutorResponse.decode(value),
   },
   serverConnectionCheck: {
-    path: "/inferenceServiceServer.InferenceService/ServerConnectionCheck",
+    path: "/inferenceServiceServer.InferenceService/serverConnectionCheck",
     requestStream: false,
     responseStream: false,
-    requestSerialize: (value: ReturnConnectedCheck) =>
-      Buffer.from(ReturnConnectedCheck.encode(value).finish()),
+    requestSerialize: (value: ReturnConnectedCheck) => Buffer.from(ReturnConnectedCheck.encode(value).finish()),
     requestDeserialize: (value: Buffer) => ReturnConnectedCheck.decode(value),
-    responseSerialize: (value: ConnectedCheckResponse) =>
-      Buffer.from(ConnectedCheckResponse.encode(value).finish()),
-    responseDeserialize: (value: Buffer) =>
-      ConnectedCheckResponse.decode(value),
+    responseSerialize: (value: ConnectedCheckResponse) => Buffer.from(ConnectedCheckResponse.encode(value).finish()),
+    responseDeserialize: (value: Buffer) => ConnectedCheckResponse.decode(value),
   },
 } as const;
 
 export interface InferenceServiceServer extends UntypedServiceImplementation {
   getChatResponse: handleServerStreamingCall<ChatDetails, TutorResponse>;
-  serverConnectionCheck: handleUnaryCall<
-    ReturnConnectedCheck,
-    ConnectedCheckResponse
-  >;
+  serverConnectionCheck: handleUnaryCall<ReturnConnectedCheck, ConnectedCheckResponse>;
 }
 
 export interface InferenceServiceClient extends Client {
-  getChatResponse(
-    request: ChatDetails,
-    options?: Partial<CallOptions>
-  ): ClientReadableStream<TutorResponse>;
+  getChatResponse(request: ChatDetails, options?: Partial<CallOptions>): ClientReadableStream<TutorResponse>;
   getChatResponse(
     request: ChatDetails,
     metadata?: Metadata,
-    options?: Partial<CallOptions>
+    options?: Partial<CallOptions>,
   ): ClientReadableStream<TutorResponse>;
   serverConnectionCheck(
     request: ReturnConnectedCheck,
-    callback: (
-      error: ServiceError | null,
-      response: ConnectedCheckResponse
-    ) => void
+    callback: (error: ServiceError | null, response: ConnectedCheckResponse) => void,
   ): ClientUnaryCall;
   serverConnectionCheck(
     request: ReturnConnectedCheck,
     metadata: Metadata,
-    callback: (
-      error: ServiceError | null,
-      response: ConnectedCheckResponse
-    ) => void
+    callback: (error: ServiceError | null, response: ConnectedCheckResponse) => void,
   ): ClientUnaryCall;
   serverConnectionCheck(
     request: ReturnConnectedCheck,
     metadata: Metadata,
     options: Partial<CallOptions>,
-    callback: (
-      error: ServiceError | null,
-      response: ConnectedCheckResponse
-    ) => void
+    callback: (error: ServiceError | null, response: ConnectedCheckResponse) => void,
   ): ClientUnaryCall;
 }
 
 export const InferenceServiceClient = makeGenericClientConstructor(
   InferenceServiceService,
-  "inferenceServiceServer.InferenceService"
+  "inferenceServiceServer.InferenceService",
 ) as unknown as {
-  new (
-    address: string,
-    credentials: ChannelCredentials,
-    options?: Partial<ClientOptions>
-  ): InferenceServiceClient;
+  new (address: string, credentials: ChannelCredentials, options?: Partial<ClientOptions>): InferenceServiceClient;
   service: typeof InferenceServiceService;
 };
 
-type Builtin =
-  | Date
-  | Function
-  | Uint8Array
-  | string
-  | number
-  | boolean
-  | undefined;
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
 
-export type DeepPartial<T> = T extends Builtin
-  ? T
-  : T extends Array<infer U>
-  ? Array<DeepPartial<U>>
-  : T extends ReadonlyArray<infer U>
-  ? ReadonlyArray<DeepPartial<U>>
-  : T extends {}
-  ? { [K in keyof T]?: DeepPartial<T[K]> }
+export type DeepPartial<T> = T extends Builtin ? T
+  : T extends Array<infer U> ? Array<DeepPartial<U>> : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
+  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
 
 type KeysOfUnion<T> = T extends T ? keyof T : never;
-export type Exact<P, I extends P> = P extends Builtin
-  ? P
-  : P & { [K in keyof P]: Exact<P[K], I[K]> } & {
-      [K in Exclude<keyof I, KeysOfUnion<P>>]: never;
-    };
+export type Exact<P, I extends P> = P extends Builtin ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
 
 function isObject(value: any): boolean {
   return typeof value === "object" && value !== null;
