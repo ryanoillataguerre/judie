aca5470bc816010b1b9983c6e9bfff6659492964
bcormier1 <bcormier@uci.edu>
2023-09-26 15:13:48 -0700

    testing

--- services/inference_service/prompts/prompt_generator.py
+++ services/inference_service/prompts/prompt_generator.py
@@ -7,7 +7,7 @@ from typing import List, Optional
 
 def assemble_prompt_chunks(subject: str, user_type: Optional[str] = None):
     if subject in prompt_chunks.NON_TUTOR_SUBJECTS:
-        chunks = prompt_chunks[subject]
+        chunks = prompt_chunks.PROMPT_MAP[subject]
     else:
         user_chunk = prompt_chunks.STUDENT_TUTOR_CHUNK
         if user_type:
@@ -15,10 +15,11 @@ def assemble_prompt_chunks(subject: str, user_type: Optional[str] = None):
                 user_chunk = prompt_chunks.PARENT_TUTOR_CHUNK
 
         chunks = user_chunk
-        chunks += prompt_chunks.PROMPT_MAP[subject]
+        if prompt_chunks in prompt_chunks.PROMPT_MAP:
+            chunks = "  ".join([chunks, prompt_chunks.PROMPT_MAP[subject]])
 
         if subject in prompt_chunks.MATH_SUBJECTS:
-            chunks += prompt_chunks.MATH_CHUNK
+            chunks = "  ".join([chunks, prompt_chunks.MATH_CHUNK])
 
     return chunks
 
--- services/inference_service/prompts/test_prompt_generator.py
+++ services/inference_service/prompts/test_prompt_generator.py
@@ -1,5 +1,5 @@
 import pytest
-from inference_service.prompts.prompt_generator import generate_question_answer_prompt
+from inference_service.prompts import prompt_generator
 from inference_service.context.context_retriever import (
     pull_context_block,
     CONTEXT_LIMIT,
@@ -21,21 +21,21 @@ def env_setup():
 
 
 def test_prompt_generator_e2e(env_setup):
-    full_prompt = generate_question_answer_prompt(
+    full_prompt = prompt_generator.generate_question_answer_prompt(
         question="Teach me science", subject=None
     )
     assert len(full_prompt) > 0
 
 
 def test_subject_prompt(env_setup):
-    full_prompt = generate_question_answer_prompt(
+    full_prompt = prompt_generator.generate_question_answer_prompt(
         question="What is photosynthesis?", subject="AP Biology"
     )
     assert "bio" in full_prompt
 
 
 def test_special_context_prompt(env_setup):
-    full_prompt = generate_question_answer_prompt(
+    full_prompt = prompt_generator.generate_question_answer_prompt(
         question="What is the derivative of 3x^5?",
         subject="AP Calculus AB",
     )
@@ -57,3 +57,19 @@ def test_context_limit(env_setup):
         len(pull_context_block(query="Teach me science", subject="AP Biology"))
         <= CONTEXT_LIMIT
     )
+
+def test_chunk_assembly(env_setup):
+    prompt = prompt_generator.assemble_prompt_chunks(subject="AP Art History")
+    assert "tutor" in prompt
+
+def test_chunk_assembly_parent(env_setup):
+    prompt = prompt_generator.assemble_prompt_chunks(subject="AP Art History",user_type="PARENT")
+    assert "parent" in prompt
+
+def test_chunk_assembly_math(env_setup):
+    prompt = prompt_generator.assemble_prompt_chunks(subject="Geometry")
+    assert "calculations" in prompt
+
+def test_chunk_assembly_non_tutor(env_setup):
+    prompt = prompt_generator.assemble_prompt_chunks(subject="Testing")
+    assert "tutor" not in prompt
