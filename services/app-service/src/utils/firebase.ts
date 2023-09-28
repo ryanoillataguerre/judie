84385750fdb3903040de8e33da02b90dc5033a64
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-09-28 15:38:19 -0700

    Update firebase test config

--- services/app-service/src/utils/firebase.ts
+++ services/app-service/src/utils/firebase.ts
@@ -7,7 +7,9 @@ const getFirebaseConfig = () => {
   const config: firebase.AppOptions = {};
   const env = getEnv();
   if (env === Environment.Local) {
-    config.credential = firebase.credential.cert(JSON.stringify(json));
+    config.credential = firebase.credential.cert(
+      JSON.stringify(json) || (process.env.FIREBASE_LOCAL_AUTH as string)
+    );
     config.databaseURL = "https://sandbox-382905.firebaseio.com";
   }
   // If deployed, no auth is needed - env var GOOGLE_CLOUD_PROJECT is set and VPC works correctly
