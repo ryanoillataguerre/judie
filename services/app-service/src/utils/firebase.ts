992a68eb678cd1757fe2e1edb8870f151feb5b6f
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2023-09-28 15:52:52 -0700

    Update

--- services/app-service/src/utils/firebase.ts
+++ services/app-service/src/utils/firebase.ts
@@ -1,14 +1,16 @@
 import * as firebase from "firebase-admin";
 const { initializeApp } = firebase;
 import { Environment, getEnv } from "./env.js";
-import json from "./firebase-local-auth.json";
 
 const getFirebaseConfig = () => {
   const config: firebase.AppOptions = {};
   const env = getEnv();
   if (env === Environment.Local || env === Environment.Test) {
     config.credential = firebase.credential.cert(
-      JSON.stringify(json) || (process.env.FIREBASE_LOCAL_AUTH as string)
+      (process.env.FIREBASE_LOCAL_AUTH as string) ||
+        JSON.stringify(
+          import("./firebase-local-auth.json").then((m) => m.default)
+        )
     );
     config.databaseURL = "https://sandbox-382905.firebaseio.com";
   }
