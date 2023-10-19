10a3a28b1afe64fcd9a06cc1b8ff70b0516b1f91
Rowan JudieAI <wingr-judieai@rowans-mbp-2.lan>
2023-10-19 13:25:23 -0600

    feat: adding functionality to gcp_utils and cleaning up

--- notebooks/rowan/src/gcp_utils.py
+++ notebooks/rowan/src/gcp_utils.py
@@ -1,7 +1,7 @@
 """
 This module allows for running queries against BigQuery.
 
-Date: 2023-10-16 (modified 2023-10-16)
+Date: 2023-10-16 (modified 2023-10-19)
 """
 import json
 import logging
@@ -10,10 +10,15 @@ from io import BytesIO
 from typing import Optional
 
 import pandas as pd
+import pyarrow
+from google.api_core.exceptions import GoogleAPICallError
 from google.cloud import bigquery, bigquery_storage, storage
 from jinja2 import Template
 
 from src.general_utils import get_logger
+
+
+ALLOWED_FILE_FORMATS = ('csv', 'parquet')  # Move to enum or config later
 
 
 class GCPUtils:
@@ -35,6 +40,23 @@ class GCPUtils:
         """
         self.gcp_project_id = gcp_project_id
 
+    def _is_valid_file_format(self, file_format: str) -> bool:
+        """Validate that passed in file_format is one of the allowed formats.
+
+        Args:
+            file_format : The file format to check, e.g. 'csv', 'parquet'
+        Return:
+            True/False depending on whether file_format in ALLOWED_FILE_FORMATS list
+        """
+        if file_format.lower() not in ALLOWED_FILE_FORMATS:
+            self.logger.error(
+                f"Invalid file_format ({file_format.lower()})\n",
+                f"file_format must be one of {ALLOWED_FILE_FORMATS}"
+            )
+            return False
+        else:
+            return True
+
 
 class GCSUtils(GCPUtils):
     """Class that defines utility functions for GCP Cloud Storage.
@@ -51,8 +73,21 @@ class GCSUtils(GCPUtils):
             gcp_project_id  : The GCP project-id with which to interact
             logger          : Optional logger to use when displaying logs
         """
-        self.set_gcp_project_id(gcp_project_id)
         self.set_logger(logger)
+        self.set_gcp_project_id(gcp_project_id)
+
+    def _get_storage_client(self) -> storage.Client:
+        """Instantiate a GCS storage client.
+
+        Return
+            GSC storage client object.
+        """
+        try:
+            return storage.Client(project=self.gcp_project_id)
+        except GoogleAPICallError as e:
+            self.logger.exception(f"Failed to initialize GSC storage client: {e}")
+        except Exception as e:
+            self.logger.exception(f"An unexpected error occurred: {e}")
 
     def write_pd_dataframe_to_gcs(
         self, df: pd.DataFrame, bucket: str, prefix: str, file_format: str="parquet",
@@ -65,26 +100,36 @@ class GCSUtils(GCPUtils):
             prefix      : The gcs path within the bucket
             file_format : The file format to use when writing
         """
+        if not self._is_valid_file_format(file_format):
+            self.logger.error(f"File not written because of invalid file_format.")
+            return
+
         self.logger.info(f"Writing dataframe to {bucket}/{prefix} as a {file_format} file ...")
-        client = storage.Client(project=self.gcp_project_id)
+        client = self._get_storage_client()
         bucket = client.bucket(bucket)
         blob = bucket.blob(prefix)
 
-        # Convert dataframe to in-memory bytes buffer
+        # Use in-memory bytes buffer to Convert dataframe
         buffer = BytesIO()
 
         # Save DataFrame to a Parquet format and upload to GCS
-        if file_format == "parquet":
-            df.to_parquet(buffer, index=False, compression='snappy')
-        elif file_format == "csv":
+        if file_format == "csv":
             df.to_csv(buffer, index=False)
+        elif file_format == "parquet":
+            df.to_parquet(buffer, index=False, compression='snappy')
         else:
             raise ValueError(f"Unsupported file format: {file_format}")
 
-        # Create a blob and upload the file's content (seek buffer to beginning)
-        buffer.seek(0)
-        blob = bucket.blob(prefix)
-        blob.upload_from_file(buffer, content_type='application/octet-stream')
+        try:
+            # Create a blob and upload the file's content (seek buffer to beginning)
+            buffer.seek(0)
+            blob = bucket.blob(prefix)
+            blob.upload_from_file(buffer, content_type='application/octet-stream')
+        except GoogleAPICallError as e:
+            self.logger.exception(f"Failed to write data: {e}")
+        except Exception as e:
+            self.logger.exception(f"An unexpected error occurred: {e}")
+
         self.logger.info(f"Dataframe written successfully.")
 
 
@@ -103,8 +148,34 @@ class BQUtils(GCPUtils):
             gcp_project_id  : The GCP project-id with which to interact
             logger          : Optional logger to use for logging
         """
-        self.set_gcp_project_id(gcp_project_id)
         self.set_logger(logger)
+        self.set_gcp_project_id(gcp_project_id)
+
+    def _get_bq_client(self) -> bigquery.Client:
+        """Instantiate a BigQuery client.
+
+        Return
+            BigQuery client object.
+        """
+        try:
+            return bigquery.Client(project=self.gcp_project_id)
+        except GoogleAPICallError as e:
+            self.logger.exception(f"Failed to initialize BigQuery client: {e}")
+        except Exception as e:
+            self.logger.exception(f"An unexpected error occurred: {e}")
+
+    def _get_bq_storage_client(self) -> bigquery_storage.BigQueryReadClient:
+        """Instantiate a BigQueryReadClient.
+
+        Return
+            BigQueryReadClient object.
+        """
+        try:
+            return bigquery_storage.BigQueryReadClient()
+        except GoogleAPICallError as e:
+            self.logger.exception(f"Failed to initialize BigQueryReadClient: {e}")
+        except Exception as e:
+            self.logger.exception(f"An unexpected error occurred: {e}")
 
     def get_query_from_file(self, query_file: str) -> str:
         """Read query from a file and return the query string.
@@ -118,12 +189,11 @@ class BQUtils(GCPUtils):
         try:
             with open(query_file, "rt") as f:
                 query_ls_tmp = f.readlines()
-        except Exception as e:
+        except (IOError, FileNotFoundError) as e:
             query_ls_tmp = []
             custom_msg = (f"Problems reading query from {query_file}\n"
-                          f"Returning None for query_ls")
+                          f"Returning empty list for query_ls")
             self.logger.exception(custom_msg)
-            raise
 
         # Remove empty lines
         query_ls = [l for l in query_ls_tmp if l.strip() != ""]
@@ -167,37 +237,158 @@ class BQUtils(GCPUtils):
         formatted_qstr = Template(query).render(params=params or {})
         return formatted_qstr
 
-    def run_query_to_df(self, query_file: str, query_params_dc: dict) -> pd.DataFrame:
+    def run_query(
+        self,
+        query_file: str,
+        query_params: dict,
+        job_config: Optional[bigquery.QueryJobConfig]=None,
+    ) -> bigquery.job.query.QueryJob:
+        """Run a query against BigQuery.
+
+        Args:
+            query_file      : Path to query file to run
+            query_params    : Parameters to use when completing the query template
+            job_config      : Optional job configurations for the query
+        Return:
+            QueryJob object
+        """
+        self.logger.info(f"Reading query from {query_file} and templating ...")
+        query = self.template_query(query_file, query_params)
+
+        self.logger.info("Instantiating bigquery.Client() ...")
+        client = self._get_bq_client()
+
+        self.logger.info(f"Running query ...\n{query}")
+        try:
+            if job_config:
+                query_job = client.query(query, job_config=job_config)
+            else:
+                query_job = client.query(query)
+            query_job.result()  # Wait for the query to complete
+
+            if query_job.errors:
+                for error in query_job.errors:
+                    self.logger.error(f"Error: {error['message']}")
+            else:
+                self.logger.info("Query completed successfully.")
+
+        except GoogleAPICallError as e:
+            self.logger.exception(f"An error occurred while querying or fetching the data: {e}")
+
+        except Exception as e:
+            self.logger.exception(f"An unexpected error occurred: {e}")
+
+        return query_job
+
+    def run_query_to_df(self, query_file: str, query_params: dict) -> pd.DataFrame:
         """Run query and return results in a Pandas DataFrame.
 
         Args:
             query_file      : Path to query file to run
-            query_params_dc : Parameters to use when completing the query template
+            query_params    : Parameters to use when completing the query template
         Return:
             df              : Results in a dataframe
         """
-        self.logger.info(f"Reading query from {query_file} and templating ...")
-        query = self.template_query(query_file, query_params_dc)
+        # Run query
+        query_job = self.run_query(query_file, query_params)
 
-        self.logger.info("Instantiating bigquery.Client() and bigquery_storage clients ...")
+        # Convert results to pd.DataFrame
+        self.logger.info("Instantiating BigQueryReadClient ...")
+        bqstorage_client = self._get_bq_storage_client()
         try:
-            client = bigquery.Client(project=self.gcp_project_id)
-            bqstorage_client = bigquery_storage.BigQueryReadClient()
-        except OSError:
-            self.logger.exception("Issue creating BQ clients")
-            raise
+            self.logger.info("Converting results to dataframe ...")
+            df = query_job.to_arrow(bqstorage_client=bqstorage_client).to_pandas()
 
-        self.logger.info(f"Running query ...\n{query}")
-        try:
-            query_job = client.query(query)
-        except Exception:
-            self.logger.exception(e)
-            raise
+        except pyarrow.ArrowInvalid as e:
+            self.logger.exception(f"An error occurred converting the data to a pd.DataFrame: {e}")
 
-        self.logger.info("Converting results to dataframe ...")
-        df = query_job.to_arrow(bqstorage_client=bqstorage_client).to_pandas()
-        #df = query_job.to_dataframe()
         self.logger.info(f"Results in df with shape: {df.shape}")
         self.logger.info(f"\n{df.head()}")
         return df
+
+    def run_query_to_bq_table(
+        self,
+        query_file: str,
+        query_params: dict,
+        dataset: str,
+        table: str
+    ) -> None:
+        """Run query and save results in a BigQuery dataset.table.
+
+        Args:
+            query_file      : Path to query file to run
+            query_params    : Parameters to use when completing the query template
+            dataset         : The dataset in BigQuery to which to save the data 
+            table           : The table in BigQuery to which to save the data
+        """
+        # Initialize a BigQuery client
+        client = self._get_bq_client()
+
+        # Specify the destination table
+        table_ref = client.dataset(dataset).table(table)
+        job_config = bigquery.QueryJobConfig()
+        job_config.destination = table_ref
+
+        # Run query
+        query_job = self.run_query(query_file, query_params, job_config)
+
+    def extract_bq_table_to_gcs(
+        self,
+        dataset: str,
+        table: str,
+        bucket: str,
+        prefix: str,
+        file_format: str='csv',
+    ) -> None:
+        """Run a query and export results to specified bucket/prefix.
+
+        Args:
+            dataset     : The dataset in BigQuery that has the data
+            table       : The table in BigQuery with the data to extract
+            bucket      : The bucket for the query results
+            prefix      : The full file prefix including file extension
+            file_format : Which file format to use when writing results, e.g. 'csv' or 'parquet'
+
+        """
+        if not self._is_valid_file_format(file_format):
+            self.logger.error(f"Query not run because of invalid file_format.")
+            return
+
+        self.logger.info(
+            f"Exporting query results to {bucket}/{prefix} as a {file_format} file ..."
+        )
+
+        # Initialize a BigQuery client
+        client = self._get_bq_client()
+
+        # Specify the destination URI, format, and other export options
+        destination_uri = f"gs://{bucket}/{prefix}"
+        table_ref = client.dataset(dataset).table(table)
+
+        extract_job_config = bigquery.job.ExtractJobConfig()
+        extract_job_config.destination_format = (bigquery.DestinationFormat.CSV
+                                                 if file_format.lower() == 'csv'
+                                                 else bigquery.DestinationFormat.PARQUET)
+
+        try:
+            # Start the export job
+            extract_job = client.extract_table(
+                table_ref,
+                destination_uri,
+                job_config=extract_job_config
+            )
+            extract_job.result()  # Wait for the job to complete
+
+            # Check for errors
+            if extract_job.errors:
+                for error in extract_job.errors:
+                    self.logger.error(f"Error: {error['message']}")
+            else:
+                self.logger.info("Export job completed successfully")
+
+        except GoogleAPICallError as e:
+            self.logger.exception(f"Failed to initiate export job: {e}")
+
+        except Exception as e:
+            self.logger.exception(f"An unexpected error occurred: {e}")
 
